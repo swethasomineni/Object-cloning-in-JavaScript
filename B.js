f2917c8b7f0f3f0418d397db46a27fad2bca1f32
avinashchennupati <chennupatiavinash99@gmail.com>
2021-06-26 11:35:18 +0530

    object cloning first commit

---
+++ B.js
@@ -0,0 +1,11 @@
+let arr = [10, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let user = {
+  name: "jayanth",
+  age: 27,
+  wife: "swetha",
+};
+
+for (const key in user) {
+  console.log(`${key}: ${user[key]}`);
+}
